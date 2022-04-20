c03a53ad39c9370191d57ce0cfadc669fba3ffce
Gabriel Ilie <gabriel.ilie@velocitor.com>
2022-04-20 15:13:04 -0400

    Refactored

---
+++ DontSleep/app/src/main/java/com/ilieinc/dontsleep/ui/compose/component/Dialog.kt
@@ -0,0 +1,102 @@
+package com.ilieinc.dontsleep.ui.compose.component
+
+import android.app.Activity
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalContext
+import com.ilieinc.dontsleep.viewmodel.*
+import com.ilieinc.dontsleep.viewmodel.base.CardViewModel
+import com.ilieinc.dontsleep.viewmodel.base.HelpDialogViewModel
+import com.ilieinc.dontsleep.viewmodel.base.PermissionDialogViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+@Composable
+fun CardHelpDialog(
+    showDialog: MutableStateFlow<Boolean>,
+    viewModel: CardViewModel
+) {
+    val activity = LocalContext.current as Activity
+    when (viewModel) {
+        is DontSleepCardViewModel -> {
+            HelpDialog(
+                DontSleepHelpDialogViewModel(
+                    showDialog,
+                    activity.application
+                )
+            )
+        }
+        is SleepCardViewModel -> {
+            HelpDialog(
+                SleepCardHelpDialogViewModel(
+                    showDialog,
+                    activity.application
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun CardPermissionDialog(
+    showPermissionDialog: MutableStateFlow<Boolean>,
+    viewModel: CardViewModel
+) {
+    val activity = LocalContext.current as Activity
+    when (viewModel) {
+        is DontSleepCardViewModel -> {
+            PermissionDialog(
+                DontSleepPermissionDialogViewModel(
+                    showPermissionDialog,
+                    activity.application
+                )
+            )
+        }
+        is SleepCardViewModel -> {
+            PermissionDialog(
+                SleepPermissionDialogViewModel(
+                    showPermissionDialog,
+                    activity
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun HelpDialog(viewModel: HelpDialogViewModel) {
+    AlertDialog(onDismissRequest = { viewModel.showDialog.tryEmit(false) },
+        title = { Text(viewModel.title.collectAsState().value) },
+        text = { Text(viewModel.description.collectAsState().value) },
+        confirmButton = { Button(onClick = { viewModel.showDialog.tryEmit(false) }) { Text("OK") } },
+        dismissButton = {
+            if (viewModel.showRevokePermissionButton.collectAsState().value) {
+                Button(onClick = { viewModel.revokePermission() }) {
+                    Text(text = viewModel.revokeButtonText.collectAsState().value)
+                }
+            }
+        })
+}
+
+@Composable
+fun PermissionDialog(viewModel: PermissionDialogViewModel) {
+    AlertDialog(
+        onDismissRequest = { viewModel.showDialog.tryEmit(false) },
+        title = { Text(viewModel.title.collectAsState().value) },
+        text = { Text(viewModel.description.collectAsState().value) },
+        confirmButton = {
+            Button(
+                onClick = { viewModel.requestPermission() },
+                enabled = viewModel.confirmButtonEnabled.collectAsState().value
+            ) {
+                Text(viewModel.confirmButtonText.collectAsState().value)
+            }
+        },
+        dismissButton = {
+            Button(onClick = { viewModel.showDialog.tryEmit(false) }) {
+                Text("No")
+            }
+        })
+}
